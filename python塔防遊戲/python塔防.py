24d4c27359342caf54c91ab670c64b541bbc252e
WhiteKing0000 <150752606+WhiteKing0000@users.noreply.github.com>
2024-02-29 14:58:47 +0800

    嘗試做出開始按鈕功能

--- python塔防遊戲/python塔防.py
+++ python塔防遊戲/python塔防.py
@@ -1,17 +1,23 @@
+#在mac不是用pip是pip3
 # 載入遊戲套件
 import pygame
 import os
-
+import pyautogui
+import sys
 
 
 # 遊戲基本變數設定
 FPS = 60
 pygame.init()
 white = (255, 255, 255)
+WIN_WIDTH, WIN_HEIGHT = 800, 600
 WIDTH = 1200
 HEIGHT = 700
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+#字體
+font = pygame.font.SysFont(None, 40)
 
 #遊戲視窗初始化
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -29,11 +35,11 @@ startb = pygame.image.load(os.path.join('遊戲素材', '開始按鈕.png')).con
 
 #設定敵方進攻路徑素材
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(road_img, (800, 700))
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.transform.scale(road_img, (800, 700))
+            self.image.set_colorkey(BLACK)
+            self.rect = self.image.get_rect()
 
 
 class background(pygame.sprite.Sprite):
@@ -43,41 +49,62 @@ class background(pygame.sprite.Sprite):
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
 
+#定義按鈕類物件
+class button(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, color, text=''):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.text = text
 
-class startbutton(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(startb, (800, 700))
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+        if self.text != '':
+            font_surface = font.render(self.text, True, BLACK)
+            win.blit(font_surface, (self.rect.x + self.rect.width/2 - font_surface.get_width()/2, 
+                                    self.rect.y + self.rect.height/2 - font_surface.get_height()/2))
+
+    def is_over(self, pos):
+        return self.rect.collidepoint(pos)
 
 
 
 # 建立角色陣列並將角色放入陣列
 all_sprites = pygame.sprite.Group()
+#建立按鈕
+Button = button(800, 0, 200, 100, GRAY, 'press me')
 #Player()
 all_sprites.add(Player())
 all_sprites.add(background())
-all_sprites.add(startbutton())
 
-button_rect = startbutton.rect
+
+
+
 # 遊戲執行迴圈
 running = True
 while running:
-    #取得輸入
+    #關掉遊戲視窗
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        #print('mouse button down at:', mouse_x, mouse_y)
-        if button_rect.collidepoint(mouse_x, mouse_y):
-            print('mouse clicked on the object')
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            #如果滑鼠點擊按鈕
+            if Button.is_over(pygame.mouse.get_pos()):
+                Button.color = BLACK
+                text = font.render('game start!!!', True, BLACK)
+                text_rect = text.get_rect(center=(WIDTH/2, HEIGHT/2))
+                screen.blit(text, text_rect)
+                pygame.display.flip()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            Button.color = GRAY
+
+    #偵測滑鼠是否碰到並按下按鈕
+    
     #更新遊戲
     all_sprites.update()
     #畫面顯示
     screen.fill((191, 239, 245))
     all_sprites.draw(screen)
+    Button.draw(screen)
     pygame.display.update()
 
 pygame.quit()
